e395a90426769a9cb4eef83aecd5eef135d349c2
miladbehrooz <milad.behrooz68@gmail.com>
2025-06-10 23:14:15 +0200

    feat: Add different CleaningHandlers

--- backend/etl/preprocessing/cleaning_data_handlers.py
+++ backend/etl/preprocessing/cleaning_data_handlers.py
@@ -1,10 +1,21 @@
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 
-from .operations import clean_text
 from backend.etl.domain.documents import Document
-from backend.etl.domain.cleaned_documents import CleanedDocument, CleanedArticleDocument
-from backend.etl.domain.documents import ArticleDocument
+from backend.etl.domain.cleaned_documents import (
+    CleanedDocument,
+    CleanedArticleDocument,
+    CleanedRepositoryDocument,
+    CleanedYoutubeDocument,
+    PDFCleanedDocument,
+)
+from backend.etl.domain.documents import (
+    ArticleDocument,
+    RepositoryDocument,
+    YoutubeDocument,
+    PDFDocument,
+)
+from .operations import clean_text
 
 
 DocumentT = TypeVar("DocumentT", bound=Document)
@@ -27,4 +38,37 @@ class ArticleCleaningHandler(CleaningDataHandler):
             content=clean_text(" #### ".join(valid_content)),
             platform=data_model.platform,
             link=data_model.link,
+        )
+
+
+class RepositoryCleaningHandler(CleaningDataHandler):
+    def clean(self, data_model: RepositoryDocument) -> CleanedRepositoryDocument:
+        return CleanedRepositoryDocument(
+            id=data_model._id,
+            content=clean_text(" #### ".join(data_model.content.values())),
+            platform=data_model.platform,
+            name=data_model.name,
+            link=data_model.link,
+        )
+
+
+class YoutubeCleaningHandler(CleaningDataHandler):
+    def clean(self, data_model: YoutubeDocument) -> CleanedYoutubeDocument:
+        return CleanedDocument(
+            id=data_model._id,
+            content=clean_text(" #### ".join(data_model.content.values())),
+            platform=data_model.platform,
+            name=data_model.name,
+            link=data_model.link,
+        )
+
+
+class PDFCleaningHandler(CleaningDataHandler):
+    def clean(self, data_model: PDFDocument) -> PDFCleanedDocument:
+        return CleanedDocument(
+            id=data_model._id,
+            content=clean_text(" #### ".join(data_model.content.values())),
+            platform=data_model.platform,
+            name=data_model.name,
+            path=data_model.path,
         )
