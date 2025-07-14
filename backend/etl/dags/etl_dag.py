d59fa18439088b58887222a9953f1b1d6dba2e2b
miladbehrooz <milad.behrooz68@gmail.com>
2025-07-14 21:30:27 +0200

    feat: update the etl dag to avoid unnecessary runs

--- backend/etl/dags/etl_dag.py
+++ backend/etl/dags/etl_dag.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from airflow.decorators import dag
+from airflow.decorators import dag, task
 from backend.etl.tasks.extract import extract_sources
 from backend.etl.tasks.query_data_warehouse import query_data_warehouse
 from backend.etl.tasks.clean import clean_documents
@@ -15,21 +15,29 @@ from backend.utils import logger
     catchup=False,
     tags=["etl"],
 )
-def etl_pipeline(batch_id: str = "batch_002"):
-    is_extracted = extract_sources(
-        [
-            "https://weaviate.io/blog/advanced-rag",
-            # "https://www.youtube.com/watch?v=T-D1OfcDW1M&t=5s",
-            # "backend/data/book.pdf",
-            # "https://github.com/PacktPublishing/LLM-Engineers-Handbook.git",
-        ],
-        batch_id=batch_id,
-    )
+def etl_pipeline(sources: list, batch_id: str = "batch_003"):
+    @task.short_circuit
+    def check_new_extraction(is_extracted: bool) -> bool:
+        if not is_extracted:
+            logger.info("No new data extracted. Skipping the rest of the pipeline.")
+        return is_extracted
 
-    documents = query_data_warehouse(batch_id, is_extracted=is_extracted)
+    is_extracted = extract_sources(sources=sources, batch_id=batch_id)
+    new_extraction = check_new_extraction(is_extracted)
+    documents = query_data_warehouse(batch_id, new_extraction=new_extraction)
     cleaned_documents = clean_documents(documents)
     embedded_chunks = chunk_and_embed_documents(cleaned_documents)
     load_to_vector_db(embedded_chunks)
 
 
-etl_pipeline_dag = etl_pipeline()
+sources = [
+    # "https://weaviate.io/blog/advanced-rag",
+    "https://www.pinecone.io/learn/vector-database/",
+    "https://python.langchain.com/docs/integrations/vectorstores/qdrant/",
+    "https://en.wikipedia.org/wiki/Apache_Airflow",
+    "https://datascientest.com/en/apache-airflow-what-is-it",
+    # "https://www.youtube.com/watch?v=T-D1OfcDW1M&t=5s",
+    # "backend/data/book.pdf",
+    # "https://github.com/PacktPublishing/LLM-Engineers-Handbook.git",
+]
+etl_pipeline_dag = etl_pipeline(sources=sources, batch_id="batch_003")
