96d80093ef61e2254741d2c9a6eaf4610726c369
miladbehrooz <milad.behrooz68@gmail.com>
2025-07-23 22:27:16 +0200

    chore: added response models

--- backend/api/models/etl.py
+++ backend/api/models/etl.py
@@ -1,6 +1,21 @@
+from typing import Optional
 from pydantic import BaseModel
-from typing import List
 
 
 class ETLRequest(BaseModel):
-    sources: List[str]
+    sources: list[str]
+
+
+class TriggerDAGResponse(BaseModel):
+    message: str
+    dag_id: str
+    dag_run_id: str
+    state: str
+
+
+class DAGStatusResponse(BaseModel):
+    dag_id: str
+    dag_run_id: str
+    state: str
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
